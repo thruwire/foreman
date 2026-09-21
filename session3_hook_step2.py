ffa70330e8c0cd1396738fcc80b9f1099ae479d0
Tydin Jarman <tjarman+ucpm@ucpm.com>
2026-09-21 15:55:06 -0700

    chore: remove one-off session scripts

--- session3_hook_step2.py
+++
@@ -1,8 +0,0 @@
-"""Locate where WORKER_COMPLETED is emitted in _run_worker to insert the
-test hook right after."""
-import re
-
-rt = open("src/foreman/runtime.py").read()
-i = rt.find("async def _run_worker")
-snippet = rt[i:i+3500]
-print(snippet)
